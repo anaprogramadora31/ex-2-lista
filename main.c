c5279108c45a868a5ae360a28380457f2101d69b
anaprogramadora31 <ana.carolinac@icloud.com>
2024-08-26 11:13:20 -0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main (){
+int n,x;
+
+printf("digite um numero inteiro e positivo:");
+scanf("%d",&n);
+
+x = 2;
+
+while (x<n){
+    if (n%x==0){
+    printf("%d nao eh primo", n);
+    break;
+}
+x++;
+}
+if (x==n){
+    printf("%d eh primo",n);
+}
+return 0;
+
+}
